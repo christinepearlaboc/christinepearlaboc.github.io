87adbc0d9dcb1df600e669c9090645f9bc1ec665
Pearl019 <christinepearlaboc442@gmail.com>
2024-09-24 18:07:44 -0700

    Second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -158,6 +158,36 @@
   }
 
   window.addEventListener("load", initSwiper);
+  
+/**
+   * Testimonials slider
+   */
+new Swiper(".testimonials-slider", {
+  speed: 600,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  slidesPerView: "auto",
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+
+    1200: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+  },
+});
+
 
   /**
    * Initiate glightbox
